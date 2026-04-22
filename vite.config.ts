44bbd0930610f01c3dc6d74881e99122004a12ab
saierukala <sai.erukala1508@gamil.com>
2026-04-23 00:30:39 +0530

    disable wayfinder for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import inertia from '@inertiajs/vite';
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
@@ -21,8 +20,8 @@ export default defineConfig({
                 },
             },
         }),
-        wayfinder({
-            formVariants: true,
-        }),
+        // wayfinder({
+        //     formVariants: true,
+        // }),
     ],
 });
